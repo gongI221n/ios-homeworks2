c726486d088c10cd4b4fab39f64d313858593d93
gong_i221n <ivan.chaschin0@gmail.com>
2022-02-16 00:28:43 +0300

    HW done

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Ivan Chaschin on 09.02.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    
+    var postButton = UIButton()
+    
+    let post = Post(title: "First post")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Feed"
+        view.backgroundColor = .systemOrange
+        
+        //Button
+        postButton = UIButton(frame: CGRect(x: view.frame.width/2-100, y: view.frame.height/2-25, width: 200, height: 50))
+        postButton.layer.cornerRadius = 10
+        postButton.backgroundColor = .systemBlue
+        postButton.setTitle("Post", for: .normal)
+        
+        postButton.addTarget(self, action: #selector(openPost), for: .touchUpInside)
+
+        view.addSubview(postButton)
+    }
+
+    // Метод для кнопки
+    @objc func openPost() {
+        let postVC = PostViewController()
+        navigationController?.pushViewController(postVC, animated: true)
+        postVC.postTitle = post.title
+    }
+    
+    
+    
+}
